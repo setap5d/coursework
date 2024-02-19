7a0157203f00b4e4aee8bd3122460b750be7807c
Vegeku <vegekusheron@gmail.com>
2024-02-19 21:25:58 +0000

    added two files

---
+++ lib/profile.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void showAccountDetails(BuildContext context) {
+  late OverlayEntry overlay;
+
+  overlay = OverlayEntry(
+    builder: (BuildContext context) => Positioned(
+      top: 0, // adjust the top position as needed
+      left: 80, // adjust the left position as needed
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.tertiary,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'User Details',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onTertiary,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Username: Jimothy',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Theme.of(context).colorScheme.onTertiary,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Email: jimothy.doe@example.com',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  color: Theme.of(context).colorScheme.onTertiary,
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  overlay.remove();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Theme.of(context).colorScheme.tertiaryContainer,
+                ),
+                child: Text(
+                  'Close',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Theme.of(context).colorScheme.onTertiaryContainer,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+  Overlay.of(context).insert(overlay);
+}
