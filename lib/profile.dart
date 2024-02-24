bb75d6fe6320d7badd08d76605b6a51dee5db5d0
fauxcat <118144566+fauxcat@users.noreply.github.com>
2024-02-24 15:22:59 +0000

    err message and name update
    user error message added, first and last name split

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,10 +9,12 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String name = '';
+  String fName = '';
+  String lName = '';
   String email = '';
   String phoneNumber = '';
   String skills = '';
+  String errMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -37,26 +39,52 @@ class _ProfilePageState extends State<ProfilePage> {
             Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  height: 64,
-                  width: MediaQuery.of(context).size.width * 0.6 * 0.9,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
+                Row(
+                  children: [
+                    Container(
+                      height: 64,
+                      width: MediaQuery.of(context).size.width * 0.3 * 0.9,
+                      padding: EdgeInsets.only(right: 16),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'First Name',
+                          border: OutlineInputBorder(),
+                        ),
+                        // inputFormatters: [
+                        //   FilteringTextInputFormatter.deny(
+                        //     RegExp(
+                        //         r"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{70,}$"),
+                        //   ),
+                        // ],
+                        onChanged: (value) {
+                          setState(() {
+                            fName = value;
+                          });
+                        },
+                      ),
                     ),
-                    // inputFormatters: [
-                    //   FilteringTextInputFormatter.deny(
-                    //     RegExp(
-                    //         r"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{70,}$"),
-                    //   ),
-                    // ],
-                    onChanged: (value) {
-                      setState(() {
-                        name = value;
-                      });
-                    },
-                  ),
+                    Container(
+                      height: 64,
+                      width: MediaQuery.of(context).size.width * 0.3 * 0.9,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Last Name',
+                          border: OutlineInputBorder(),
+                        ),
+                        // inputFormatters: [
+                        //   FilteringTextInputFormatter.deny(
+                        //     RegExp(
+                        //         r"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{70,}$"),
+                        //   ),
+                        // ],
+                        onChanged: (value) {
+                          setState(() {
+                            lName = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 16),
                 Container(
@@ -106,7 +134,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 SizedBox(height: 16),
                 Container(
-                  height: 128,
+                  height: 100,
                   width: MediaQuery.of(context).size.width * 0.6 * 0.9,
                   child: TextFormField(
                     minLines: 1,
@@ -122,12 +150,19 @@ class _ProfilePageState extends State<ProfilePage> {
                     },
                   ),
                 ),
+                SizedBox(height: 16),
+                Text(
+                  errMessage,
+                  style: TextStyle(color: Colors.red, fontSize: 16),
+                ),
+                SizedBox(height: 16),
                 ElevatedButton(
                   child: Text('Save Changes'),
                   onPressed: () {
-                    checkInputs(name, email, phoneNumber, skills);
+                    checkInputs(fName, lName, email, phoneNumber, skills);
                   },
                 ),
+                SizedBox(height: 8),
               ],
             ),
           ],
@@ -135,49 +170,53 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
     );
   }
-}
 
-void checkInputs(String name, String email, String phoneNumber, String skills) {
-  checkName(name);
-  checkEmail(email);
-  checkPhoneNumber(phoneNumber);
-  checkSkills(skills);
+  void checkInputs(String fName, String lName, String email, String phoneNumber,
+      String skills) {
+    print('#' * 40);
+    errMessage = '';
+    checkName(fName, lName);
+    checkEmail(email);
+    checkPhoneNumber(phoneNumber);
+    checkSkills(skills);
 
-  // query db here if all checks pass. otherwise show user error message
-}
+    // query db here if all checks pass. otherwise show user error message
+  }
 
-void checkName(String name) {
-  if (name.length >= 5 && name.length <= 20) {
-    print('Success! Name is between 5 and 20 characters.');
-  } else {
-    print('Error! Name is not between 5 and 20 characters.');
+  void checkName(String fName, String lName) {
+    if ((fName.length >= 2 && fName.length <= 20) &&
+        (lName.length >= 2 && lName.length <= 20)) {
+      print("Success! First and last name is between 2 and 20 characters.");
+    } else {
+      errMessage =
+          'Error! First or last name is not between 2 and 20 characters.';
+    }
+  }
+
+  void checkEmail(String email) {
+    if (email.contains('@') && email.contains('.')) {
+      print('Success! Email contains @ and .');
+    } else {
+      errMessage = 'Error! Email does not contain @ and .';
+    }
+  }
+
+  void checkPhoneNumber(String phoneNumber) {
+    if (phoneNumber.length == 10) {
+      print('Success! Phone number is 10 digits long.');
+    } else {
+      errMessage = 'Error! Phone number is not 10 digits long.';
+    }
+  }
+
+  void checkSkills(String skills) {
+    if (skills.length >= 10 && skills.length <= 100) {
+      print('Success! Skills are between 10 and 100 characters.');
+    } else {
+      errMessage = 'Error! Skills are not between 10 and 100 characters.';
+    }
   }
 }
-
-void checkEmail(String email) {
-  if (email.contains('@') && email.contains('.')) {
-    print('Success! Email contains @ and .');
-  } else {
-    print('Error! Email does not contain @ and .');
-  }
-}
-
-void checkPhoneNumber(String phoneNumber) {
-  if (phoneNumber.length == 10) {
-    print('Success! Phone number is 10 digits long.');
-  } else {
-    print('Error! Phone number is not 10 digits long.');
-  }
-}
-
-void checkSkills(String skills) {
-  if (skills.length >= 10 && skills.length <= 100) {
-    print('Success! Skills are between 10 and 100 characters.');
-  } else {
-    print('Error! Skills are not between 10 and 100 characters.');
-  }
-}
-
 
 /*
 
