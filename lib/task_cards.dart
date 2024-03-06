f6552fb92565d41c26eca0b20843634791577348
HenryAdams18 <147613897+HenryAdams18@users.noreply.github.com>
2024-03-06 14:17:50 +0000

    task page added

---
+++ lib/task_cards.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  final String taskName;
+  final String deadline;
+  final String taskAssignees;
+  final String taskDescription;
+  final List<String> ticketNames;
+  final double width;
+  final double height;
+  final bool isCardExpanded;
+  final Function(BuildContext context, int index) addTicket;
+  final int index;
+
+  const TaskCard({
+    Key? key,
+    required this.taskName,
+    required this.deadline,
+    required this.taskAssignees,
+    required this.taskDescription,
+    required this.ticketNames,
+    required this.width,
+    required this.height,
+    required this.isCardExpanded,
+    required this.addTicket,
+    required this.index,
+  }) : super(key: key);
+
+  void _handleMenuSelection(String value) {
+    switch (value) {
+      case 'Edit':
+        // Handle edit action
+        break;
+      case 'Delete':
+        // Handle delete action
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 170, 170, 170),
+                  width: 0.5,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Task Name: $taskName',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            overflow:
+                                isCardExpanded ? null : TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            'Task Assignees: $taskAssignees',
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                            overflow:
+                                isCardExpanded ? null : TextOverflow.ellipsis,
+                          ),
+                          Visibility(
+                            visible: isCardExpanded,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Task Description: $taskDescription',
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                // Display ticket names here
+                                if (ticketNames.isNotEmpty)
+                                  ...ticketNames.map((ticketName) => Text(
+                                        'Ticket: $ticketName',
+                                        style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )),
+                              ],
+                            ),
+                          ),
+                          if (isCardExpanded) const Spacer(),
+                          if (isCardExpanded)
+                            ElevatedButton(
+                              onPressed: () {
+                                addTicket(context, index);
+                              },
+                              child: const Text('Add Ticket'),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 213, 213, 213),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: SizedBox(
+                      width: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Deadline',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          const SizedBox(height: 4.0),
+                          Text(
+                            deadline,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5,
+            right: 5,
+            child: PopupMenuButton<String>(
+              onSelected: _handleMenuSelection,
+              itemBuilder: (BuildContext context) => [
+                const PopupMenuItem<String>(
+                  value: 'Edit',
+                  child: Text('Edit'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'Delete',
+                  child: Text('Delete'),
+                ),
+              ],
+              icon: const Icon(Icons.more_vert),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/task_page.dart
@@ -0,0 +1,341 @@
+import 'package:flutter/material.dart';
+import 'task_cards.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Task Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final _formKey = GlobalKey<FormState>();
+  List<dynamic> taskNames = [];
+  List<dynamic> taskAssignees = [];
+  List<dynamic> taskDescriptions = [];
+  TextEditingController taskNameController = TextEditingController();
+  TextEditingController taskAssigneesController = TextEditingController();
+  TextEditingController taskDescriptionController = TextEditingController();
+  List<bool> isCardExpanded = [];
+  List<DateTime?> deadlines = [];
+  List<List<String>> ticketNamesList = [];
+  List<List<String>> ticketDescriptionsList = [];
+
+  void _addTicket(BuildContext context, int index) async {
+    TextEditingController ticketNameController = TextEditingController();
+    TextEditingController ticketDescriptionController = TextEditingController();
+
+    await showDialog<void>(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Positioned(
+              right: -40,
+              top: -40,
+              child: InkResponse(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: const CircleAvatar(
+                  backgroundColor: Colors.red,
+                  child: Icon(Icons.close),
+                ),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      controller: ticketNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Ticket Name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          setState(() {
+                            ticketNamesList[index]
+                                .add(ticketNameController.text);
+                            ticketDescriptionsList[index]
+                                .add(ticketDescriptionController.text);
+                          });
+                          Navigator.of(context).pop();
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isCardExpanded = [];
+    deadlines = List.generate(100, (index) => null);
+    taskDescriptions = List.generate(100, (index) => '');
+    ticketNamesList = List.generate(100, (index) => []);
+    ticketDescriptionsList = List.generate(100, (index) => []);
+  }
+
+  Future<void> _selectDate(BuildContext context, int index) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != deadlines[index]) {
+      setState(() {
+        deadlines[index] = picked;
+      });
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      isCardExpanded.add(false);
+      taskNames.add(taskNameController.text);
+      taskAssignees.add(taskAssigneesController.text);
+      taskDescriptions.add(taskDescriptionController.text);
+      deadlines.add(null);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final containerWidth = screenWidth * 0.7;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Project Title'),
+      ),
+      body: Row(
+        children: [
+          SizedBox(
+            width: 200,
+            child: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ListTile(
+                    title: const Text('Item 1'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Item 2'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 700,
+                  width: containerWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 170, 170, 170),
+                      width: 0.5,
+                    ),
+                  ),
+                  child: ListView.builder(
+                    itemCount: _counter,
+                    itemBuilder: (context, index) {
+                      double cardHeight = isCardExpanded[index] ? 300.0 : 70.0;
+                      return Container(
+                        constraints: BoxConstraints(maxHeight: cardHeight),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              isCardExpanded[index] = !isCardExpanded[index];
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 0),
+                            height: cardHeight,
+                            child: Column(
+                              children: [
+                                TaskCard(
+                                  height: cardHeight,
+                                  taskName: '${taskNames[index]}',
+                                  deadline: deadlines[index] != null
+                                      ? '${deadlines[index]!.day}/${deadlines[index]!.month}/${deadlines[index]!.year}'
+                                      : 'No Deadline Set',
+                                  taskAssignees: '${taskAssignees[index]}',
+                                  taskDescription: '${taskDescriptions[index]}',
+                                  ticketNames: ticketNamesList[index],
+                                  width: 300,
+                                  isCardExpanded: isCardExpanded[index],
+                                  addTicket: _addTicket,
+                                  index: index,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog<void>(
+            context: context,
+            builder: (context) => AlertDialog(
+              content: Stack(
+                clipBehavior: Clip.none,
+                children: <Widget>[
+                  Positioned(
+                    right: -40,
+                    top: -40,
+                    child: InkResponse(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const CircleAvatar(
+                        backgroundColor: Colors.red,
+                        child: Icon(Icons.close),
+                      ),
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            controller: taskNameController,
+                            decoration: const InputDecoration(
+                              labelText: 'Task Name',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            controller: taskAssigneesController,
+                            decoration: const InputDecoration(
+                              labelText: 'Task Assignees',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: TextFormField(
+                            controller: taskDescriptionController,
+                            decoration: const InputDecoration(
+                              labelText: 'Task Description',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            child: const Text('Set Deadline'),
+                            onPressed: () {
+                              _selectDate(context, _counter);
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            child: const Text('Submit'),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+                                if (_counter < taskDescriptions.length) {
+                                  taskDescriptions[_counter] =
+                                      taskDescriptionController.text;
+                                } else {
+                                  taskDescriptions
+                                      .add(taskDescriptionController.text);
+                                }
+                                _incrementCounter();
+
+                                taskNameController.clear();
+                                taskAssigneesController.clear();
+                                taskDescriptionController.clear();
+                                Navigator.of(context).pop();
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        tooltip: 'New Task',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
